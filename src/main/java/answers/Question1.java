e136fdd1fcabcbfcea0ac2aa4f8726e1acc1b061
Jake Smith <jakesmith@Macbook-Pro.local>
2018-10-17 20:13:09 +0100

    Initial q1

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -2,9 +2,27 @@ package answers;
 
 public class Question1 {
 
+	public int merge(int a, int b)
+	{
+		return (a ^ b);
+	}
+
 	public static int bestMergedPortfolio(int[] portfolios) {
 		// TODO Auto-generated method stub
-		return -1;
+		int highest = 0;
+
+		for (int i = 0, i < portfolios.length, i+=1)
+		{
+			for (int j = 0, j < portfolios.length, j+= 1)
+			{
+				if (merge(portfolios[i],portfolios[j]) > highest)
+				{
+					highest = merge(portfolios[i],portfolios[j]);
+				}
+			}
+		}
+
+		return highest;
 	}
 
 }
