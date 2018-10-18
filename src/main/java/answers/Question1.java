87a8cb556c28ba1e784eec06b85b64591a116ac9
Jake Smith <jakesmith@dyn-10-32-133-253.winet.local>
2018-10-18 14:31:45 +0100

    Initial q1 *8

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -10,10 +10,9 @@ public class Question1 {
 
 
 
-
 		for (int i = 0; i < portfolios.length; i++)
 		{
-			for (int j = 0; j < portfolios.length; j++)
+			for (int j = i + 1; j < portfolios.length; j++)
 			{
 				if ((portfolios[i] ^ portfolios[j]) > highest)
 				{
