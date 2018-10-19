da75a3962e1e10a14c60b32a2c52bef3006e7ad8
Jake Smith <jakesmith@dyn-10-32-133-253.winet.local>
2018-10-19 13:26:37 +0100

    Initial q1 *8

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -1,15 +1,42 @@
 package answers;
+import java.lang;
 
 public class Question1 {
 
-
+    0b1110000 0b00011101
 
 	public static int bestMergedPortfolio(int[] portfolios) {
 		// TODO Auto-generated method stub
 		int highest = 0;
+		//int currentShift = 0;
+        /*
+		for (int i = 0; i < portfolios.length; i++)
+		{
+			int currentPortfolio = portfolios[i];
+			int currentBit = portfolios[i] >> 15 - currentShift;
 
+		}
+		*/
 
+        //Find most left-most 1's
+        int highestInt = 0;
+        for (int i = 0; i < portfolios.length; i++)
+        {
+            if (portfolios[i] > highestInt)
+            {
+                highestInt = portfolios[i];
+            }
+        }
 
+        for (int i = 0; i < portfolios.length; i++)
+        {
+            if ((portfolios[i] ^ portfolios[j]) > highest)
+            {
+                highest = highestInt ^ portfolios[j];
+            }
+        }
+
+        /*
 		for (int i = 0; i < portfolios.length; i++)
 		{
 			for (int j = i + 1; j < portfolios.length; j++)
@@ -20,6 +47,7 @@ public class Question1 {
 				}
 			}
 		}
+		*/
 		System.out.println("HIGHEST:"+highest);
 		return highest;
 	}
