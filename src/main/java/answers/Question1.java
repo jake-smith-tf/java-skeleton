3b31f627bf7dfcc6ac188175b09ac14a397741fc
Jake Smith <jakesmith@Macbook-Pro.local>
2018-10-22 03:03:50 +0100

    Initial q1 *15

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -6,12 +6,12 @@ public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
 	    int max = 0;
-        for (int i = 0; i < portfolios.count - 1; i++) {
-            for (int j = i + 1; j < portfolios.count; j++) {
+        for (int i = 0; i < portfolios.length - 1; i++) {
+            for (int j = i + 1; j < portfolios.length; j++) {
                 int ci = portfolios[i] ^ portfolios[j];
                 max = (ci > max) ? ci : max;
             }
         }
 	}
-	
+
 }
