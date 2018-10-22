561900516a777cf99a0c89a7b0ab7c453b7379ec
Jake Smith <jakesmith@Macbook-Pro.local>
2018-10-22 01:54:44 +0100

    Initial q6

--- src/main/java/answers/Question6.java
+++ src/main/java/answers/Question6.java
@@ -2,9 +2,43 @@ package answers;
 
 public class Question6 {
 
+	int numServers,targetServer;
+	int[][] times;
+
+	public Question6(int numServers,int targetServer,int[][] times)
+	{
+		this.numServers = numServers;
+		this.targetServer = targetServer;
+		this.times = times;
+	}
+
+
+	public int[] fastestTo(int target)
+	{
+		int last = times[0][target];
+		int lasti = 0;
+		for (int i = 0; i < numServers; i++)
+		{
+			if (times[i][target] < last)
+			{
+				last = times[i][target];
+				lasti = i;
+			}
+		}
+		return {lasti,last};
+	}
+
 	public static int shortestServerRoute(int numServers, int targetServer, int[][] times) {
-		// TODO Auto-generated method stub
-		return -1;
+		int time = 0;
+		Question6 question6 = new Question6(numServers,targetServer,times);
+		int[][] currentServer = {targetServer,0};
+		while (currentServer[0] != 0) {
+			currentServer = question6.fastestTo(currentServer[0]);
+			time += currentServer[1];
+		}
+
+
+		return time;
 	}
 
 }
