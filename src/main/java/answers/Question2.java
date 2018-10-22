7e6e7ee0dcc81e37c96232179065a2bf860446ab
Jake Smith <jakesmith@Macbook-Pro.local>
2018-10-22 01:17:43 +0100

    Initial q2

--- src/main/java/answers/Question2.java
+++ src/main/java/answers/Question2.java
@@ -3,7 +3,24 @@ package answers;
 public class Question2 {
 
 	public static int equallyBalancedCashFlow(int[] cashflowIn, int[] cashflowOut) {
-		// TODO Auto-generated method stub
+		int lowestPair = 0;
+		for (int i = 0; i < cashflowIn.length; i++)
+		{
+			int lowest = Math.abs(cashflowIn[0] - cashflowOut[0])
+			for (int j = 0; j < cashflowOut.length; j++)
+			{
+				for (int bound = 0; bound < 10; bound++) {
+					int diff = Math.abs(cashflowIn[i] - cashflowOut[j]);
+					if (diff <= bound && dif <= lowest) {
+						lowest = diff;
+					}
+				}
+			}
+			if (lowest < lowestPair)
+			{
+				lowestPair = lowest
+			}
+		}
 		return -1;
 	}
 
